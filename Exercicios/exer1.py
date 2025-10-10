36a2ff52432672f47628e2136b8650d3435017ef
Andre Lourenco <andre.lourenco.t0129853@edu.atec.pt>
2025-10-10 08:08:06 +0100

    segundoss

---
+++ Exercicios/exer1.py
@@ -0,0 +1,9 @@
+
+segundos = int(input("Introduza os segundos: "))
+
+horas = segundos // 3600
+minutos = (segundos % 3600) // 60
+
+print("Horas: ", horas)
+print("Minutos: ", minutos)
+print("Segundos: ", segundos)
