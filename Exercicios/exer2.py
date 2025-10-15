21974319f8bd1eb117de9f612335848cb33d46de
Andre Lourenco <andre.lourenco.t0129853@edu.atec.pt>
2025-10-15 14:15:45 +0100

    Exercicio para descobrir qual o maior e menor

---
+++ Exercicios/exer2.py
@@ -0,0 +1,49 @@
+"""
+valor1 = int(input("Introduza um valor: "))
+valor2 = int(input("Introduza o segundo valor: "))
+valor3 = int(input("Introduza o terceiro valor: "))
+"""
+"""
+Num1=0
+Num2=0
+Num3=0
+
+print(f"Este é o num1 {Num1} valor default")
+
+Num1 = input("Intruduza um valor")
+print (f"Num 1 valor = {Num1}")
+print(type(Num1))
+
+if Num1==12:
+    print("Inteiro")
+else:
+    print("String")
+"""
+
+valor1 = int(input("Introduza o primeiro valor: "))
+valor2 = int(input("Introduza o segundo valor: "))
+valor3 = int(input("Introduza o terceiro valor: "))
+
+#Qual o maior número entre 2 números
+"""
+if valor1>valor2:
+    print("Número 1 é o maior")
+elif valor1==valor2:
+    print("Número 1 é igual ao número 2")
+else:
+    print("Número 2 é o maior")
+"""
+
+if valor1>valor2>valor3:
+    print("Número 1 é o maior")
+elif valor2>valor3:
+    print("Número 2 é o maior")
+else:
+    print("Número 3 é o maior")
+
+if valor1<valor2<valor3:
+    print("Número 1 é o menor")
+elif valor2<valor3:
+    print("Número 2 é o menor")
+else:
+    print("Número 3 é o menor")
