6020b13b70f246f946e9f72cab59d742eaa54338
Andre Lourenco <andre.lourenco.t0129853@edu.atec.pt>
2025-10-21 13:38:15 +0100

    pedra pepel e tesoura com while e match case

---
+++ Exercicios/exer3.py
@@ -0,0 +1,22 @@
+import os
+OPC = 0
+saida = True
+
+while saida:
+    os.system('cls')
+    print("1 - para receber um bom dia")
+    print("2 - para receber um boa tarde")
+    print("3 - para receber um boa noite")
+    print("4 - Sair")
+    OPC = int(input("Intruduza a opção: "))
+
+    match OPC:
+        case 1:
+            print("Bom dia!")
+        case 2:
+            print("Boa tarde!")
+        case 3:
+            print("Boa noite!")
+        case 4:
+            print("Adeus!") 
+            break
