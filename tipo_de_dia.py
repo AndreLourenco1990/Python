c79b54867754c148592015e9eb95c05e105c34bf
Andre Lourenco <andrepedrolourenco1990@gmail.com>
2026-04-10 14:25:24 +0100

    Update tipo_de_dia.py

---
+++ tipo_de_dia.py
@@ -0,0 +1,9 @@
+dia = input("Dia: ").lower()
+
+match dia:
+    case "sábado" | "domingo":
+        print("Fim de semana")
+    case "segunda" | "terça" | "quarta" | "quinta" | "sexta":
+        print("Dia útil")
+    case _:
+        print("Dia inválido")
